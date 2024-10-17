17ebf5866fc379177ceb88f462f52e90141e06ba
Larry Hunter <larry.hunter@rfsmart.com>
2024-10-17 15:02:22 -0400

    Created Star Rating component (but did not implement)

---
+++ 07-use-popcorn/usepopcorn/src/StarRating.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import Star from './Star';
+
+const containerStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '16px',
+};
+
+const starContainerStyle = {
+  display: 'flex',
+};
+
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  className: PropTypes.string,
+  messages: PropTypes.string,
+  onSetRating: PropTypes.func,
+};
+
+export default function StarRating({
+  maxRating = 5,
+  defaultRating = 0,
+  color = '#FCC419',
+  size = 24,
+  className = '',
+  messages = [],
+  onSetRating,
+}) {
+  const [rating, setRating] = useState(defaultRating);
+  const [hoverRating, setHoverRating] = useState(0);
+
+  const handleRating = (rating) => {
+    setRating(rating);
+    onSetRating(rating);
+  };
+
+  const handleHoverRating = (hoverRating) => {
+    setHoverRating(hoverRating);
+  };
+
+  const textStyle = {
+    lineHeight: '1',
+    margin: '0',
+    color: color,
+    fontSize: `${size}px`,
+  };
+
+  return (
+    <div
+      style={containerStyle}
+      className={className}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <Star
+            key={i}
+            full={hoverRating ? hoverRating >= i + 1 : rating >= i + 1}
+            onRate={() => handleRating(i + 1)}
+            onHoverIn={() => handleHoverRating(i + 1)}
+            onHoverOut={() => handleHoverRating(0)}
+            color={color}
+            size={size}
+          />
+        ))}
+      </div>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[hoverRating ? hoverRating - 1 : rating - 1]
+          : hoverRating || rating || ''}
+      </p>
+    </div>
+  );
+}
--- 07-use-popcorn/usepopcorn/src/index.js
+++ 07-use-popcorn/usepopcorn/src/index.js
@@ -2,10 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import StarRating from './StarRating';
+
+/*
+function Test() {
+  const defaultRating = 3;
+  const [rating, setRating] = useState(defaultRating);
+  
+  return (
+    <>
+      <StarRating
+        maxRating={5}
+        defaultRating={rating}
+        color='blue'
+        onSetRating={setRating}
+        />
+      <p>This movie was rated {rating} stars!</p>
+    </>
+  );
+}
+*/
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <StarRating maxRating={10} />
+    <StarRating
+      maxRating={15}
+      defaultRating={10}
+      color='red'
+      size={42}
+      messages={['Horrible! 👎🏼', 'Not Good', 'OK', 'Pretty Good', 'Great! 👍🏼']}
+    />
+    <Test /> */}
   </React.StrictMode>
 );
