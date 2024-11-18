16fc566b78e51a31f43ed3d4a0d73c0d36efc223
Larry Hunter <larry.hunter@rfsmart.com>
2024-11-18 15:47:24 -0500

    Minor refactoring to add missing functions

--- 08-how-react-works/src/App.js
+++ 08-how-react-works/src/App.js
@@ -1,20 +1,23 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const content = [
   {
-    summary: "React is a library for building UIs",
+    id: 0,
+    summary: 'React is a library for building UIs',
     details:
-      "Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      'Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
   {
-    summary: "State management is like giving state a home",
+    id: 1,
+    summary: 'State management is like giving state a home',
     details:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
   {
-    summary: "We can think of props as the component API",
+    id: 2,
+    summary: 'We can think of props as the component API',
     details:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
   },
 ];
 
@@ -31,15 +34,34 @@ function Tabbed({ content }) {
 
   return (
     <div>
-      <div className="tabs">
-        <Tab num={0} activeTab={activeTab} onClick={setActiveTab} />
-        <Tab num={1} activeTab={activeTab} onClick={setActiveTab} />
-        <Tab num={2} activeTab={activeTab} onClick={setActiveTab} />
-        <Tab num={3} activeTab={activeTab} onClick={setActiveTab} />
+      <div className='tabs'>
+        <Tab
+          num={0}
+          activeTab={activeTab}
+          onClick={setActiveTab}
+        />
+        <Tab
+          num={1}
+          activeTab={activeTab}
+          onClick={setActiveTab}
+        />
+        <Tab
+          num={2}
+          activeTab={activeTab}
+          onClick={setActiveTab}
+        />
+        <Tab
+          num={3}
+          activeTab={activeTab}
+          onClick={setActiveTab}
+        />
       </div>
 
       {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
+        <TabContent
+          item={content.at(activeTab)}
+          key={content.at(activeTab).id}
+        />
       ) : (
         <DifferentContent />
       )}
@@ -50,9 +72,8 @@ function Tabbed({ content }) {
 function Tab({ num, activeTab, onClick }) {
   return (
     <button
-      className={activeTab === num ? "tab active" : "tab"}
-      onClick={() => onClick(num)}
-    >
+      className={activeTab === num ? 'tab active' : 'tab'}
+      onClick={() => onClick(num)}>
       Tab {num + 1}
     </button>
   );
@@ -63,29 +84,40 @@ function TabContent({ item }) {
   const [likes, setLikes] = useState(0);
 
   function handleInc() {
-    setLikes(likes + 1);
+    setLikes((likes) => likes + 1);
   }
 
+  const handleSuperLike = () => {
+    setLikes((likes) => likes + 3);
+  };
+
+  const handleUndo = () => {
+    setShowDetails(true);
+    setLikes(0);
+  };
+
+  const handleUndoLater = () => {
+    setTimeout(handleUndo, 2000);
+  };
+
   return (
-    <div className="tab-content">
+    <div className='tab-content'>
       <h4>{item.summary}</h4>
       {showDetails && <p>{item.details}</p>}
 
-      <div className="tab-actions">
-        <button onClick={() => setShowDetails((h) => !h)}>
-          {showDetails ? "Hide" : "Show"} details
-        </button>
+      <div className='tab-actions'>
+        <button onClick={() => setShowDetails((h) => !h)}>{showDetails ? 'Hide' : 'Show'} details</button>
 
-        <div className="hearts-counter">
+        <div className='hearts-counter'>
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleSuperLike}>+++</button>
         </div>
       </div>
 
-      <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+      <div className='tab-undo'>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
@@ -93,7 +125,7 @@ function TabContent({ item }) {
 
 function DifferentContent() {
   return (
-    <div className="tab-content">
+    <div className='tab-content'>
       <h4>I'm a DIFFERENT tab, so I reset state 💣💥</h4>
     </div>
   );
